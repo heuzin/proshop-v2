191f3ff560b2caa5eaacf19cba495ce7771d5413
Matheus Silva <msilvaqs@outlook.com>
2025-12-31 10:37:36 -0500

    User profile endpoints

--- backend/models/userModel.ts
+++ backend/models/userModel.ts
@@ -18,12 +18,10 @@ userSchema.methods.matchPassword = async function (enteredPassword: string) {
 };
 
 userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
-    next();
+  if (this.isModified("password")) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
   }
-
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
 });
 
 const User = mongoose.model("User", userSchema);
