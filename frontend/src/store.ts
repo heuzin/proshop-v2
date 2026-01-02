ee2967b0fd30320e5c186f98d69217fe93c55cb7
Matheus Silva <msilvaqs@outlook.com>
2026-01-02 10:11:54 -0500

    Auth & User API Slice

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice.ts";
 import cartSliceReducer from "./slices/cartSlice.ts";
+import authSliceReducer from "./slices/authSlice.ts";
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     cart: cartSliceReducer,
+    auth: authSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
