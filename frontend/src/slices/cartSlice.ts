f2a87198d713c2f07efbd53ef06beea46ca1b338
Matheus Silva <msilvaqs@outlook.com>
2026-01-03 09:54:26 -0500

    Shipping screen

--- frontend/src/slices/cartSlice.ts
+++ frontend/src/slices/cartSlice.ts
@@ -5,6 +5,8 @@ const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart")!)
   : {
       cartItems: [],
+      shippingAddress: {},
+      paymentMethod: "PayPal",
     };
 
 const cartSlice = createSlice({
@@ -33,9 +35,14 @@ const cartSlice = createSlice({
 
       return updateCart(state);
     },
+    saveShippingAddress: (state, action) => {
+      state.shippingAddress = action.payload;
+      return updateCart(state);
+    },
   },
 });
 
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, saveShippingAddress } =
+  cartSlice.actions;
 
 export default cartSlice.reducer;
