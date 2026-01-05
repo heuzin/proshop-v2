5c2c67ae4896403a570170c8816b92351055fd2e
Matheus Silva <msilvaqs@outlook.com>
2026-01-05 10:18:39 -0500

    Checkout steps component

--- frontend/src/screens/ShippingScreen.tsx
+++ frontend/src/screens/ShippingScreen.tsx
@@ -4,6 +4,7 @@ import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 
 import FormContainer from "../components/FormContainer.tsx";
+import CheckoutSteps from "../components/CheckoutSteps.tsx";
 import { saveShippingAddress } from "../slices/cartSlice.ts";
 
 const ShippingScreen = () => {
@@ -29,6 +30,7 @@ const ShippingScreen = () => {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address" className="my-2">
