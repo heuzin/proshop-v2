61af78c3401ad6867f0c2d84c9f82f09a17523b6
Matheus Silva <msilvaqs@outlook.com>
2026-01-07 10:14:10 -0500

    Order API Slice and Start Order Screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -21,6 +21,7 @@ import LoginScreen from "./screens/LoginScreen.tsx";
 import RegisterScreen from "./screens/RegisterScreen.tsx";
 import ShippingScreen from "./screens/ShippingScreen.tsx";
 import PaymentScreen from "./screens/PaymentScreen.tsx";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen.tsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -33,6 +34,7 @@ const router = createBrowserRouter(
       <Route path="" element={<PrivateRoute />}>
         <Route path="/shipping" element={<ShippingScreen />} />
         <Route path="/payment" element={<PaymentScreen />} />
+        <Route path="/placeorder" element={<PlaceOrderScreen />} />
       </Route>
     </Route>
   )
