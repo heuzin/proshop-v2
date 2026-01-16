6a26bfd963cac882ed824dd2a313c46b7582b674
Matheus Silva <msilvaqs@outlook.com>
2026-01-16 10:48:19 -0500

    Update users

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -30,6 +30,7 @@ import OrderListScreen from "./screens/admin/OrderListScreen.tsx";
 import ProductListScreen from "./screens/admin/ProductListScreen.tsx";
 import ProductEditScreen from "./screens/admin/ProductEditScreen.tsx";
 import UserListScreen from "./screens/admin/UserListScreen.tsx";
+import UserEditScreen from "./screens/admin/UserEditScreen.tsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -53,6 +54,7 @@ const router = createBrowserRouter(
         <Route path="/admin/productlist" element={<ProductListScreen />} />
         <Route path="/admin/product/:id/edit" element={<ProductEditScreen />} />
         <Route path="/admin/userlist" element={<UserListScreen />} />
+        <Route path="/admin/user/:id/edit" element={<UserEditScreen />} />
       </Route>
     </Route>
   )
---
+++ frontend/src/screens/admin/UserEditScreen.tsx
@@ -0,0 +1,117 @@
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import { toast } from "react-toastify";
+
+import Message from "../../components/Message.tsx";
+import Loader from "../../components/Loader.tsx";
+import FormContainer from "../../components/FormContainer.tsx";
+
+import {
+  useGetUserDetailsQuery,
+  useUpdateUserMutation,
+} from "../../slices/usersApiSlice.ts";
+
+const UserEditScreen = () => {
+  const { id: userId } = useParams<{ id: string }>();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  const {
+    data: user,
+    isLoading,
+    refetch,
+    error,
+  } = useGetUserDetailsQuery(userId!);
+
+  const [updateUser, { isLoading: loadingUpdate }] = useUpdateUserMutation();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      setName(user.name);
+      setEmail(user.email);
+      setIsAdmin(user.isAdmin);
+    }
+  }, [user]);
+
+  const submitHandler = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      await updateUser({ userId, name, email, isAdmin });
+      toast.success("User updated successfully");
+      refetch();
+      navigate("/admin/userlist");
+    } catch (error) {
+      toast.error((error as any).data?.message || (error as any).error);
+    }
+  };
+
+  const renderContent = () => {
+    if (isLoading) {
+      return <Loader />;
+    }
+    if (error) {
+      return (
+        <Message variant="danger">
+          {(error as any).data?.message || (error as any).error}
+        </Message>
+      );
+    }
+
+    return (
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="name" className="my-2">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="email" className="my-2">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="isadmin" className="my-2">
+          <Form.Check
+            type="checkbox"
+            label="Is Admin"
+            checked={isAdmin}
+            onChange={(e) => setIsAdmin(e.target.checked)}
+          ></Form.Check>
+        </Form.Group>
+
+        <Button type="submit" variant="primary" className="my-3">
+          Update
+        </Button>
+      </Form>
+    );
+  };
+
+  return (
+    <>
+      <Link to="/admin/userlist" className="btn btn-light my-3">
+        Go Back
+      </Link>
+      <FormContainer>
+        <h1>Edit User</h1>
+        {loadingUpdate && <Loader />}
+        {renderContent()}
+      </FormContainer>
+    </>
+  );
+};
+
+export default UserEditScreen;
